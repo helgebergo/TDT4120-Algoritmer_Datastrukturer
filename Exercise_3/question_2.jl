ec3ba15b2e5939d04e062e5246a44b9e5e3428da
Helge Bergo <Helge@dhcp-10-24-1-214.wlan.ntnu.no>
2019-09-25 16:23:57 +0200

    Forsøk med while-løkker istedenfor et skikkelig binærsøk

--- Exercise_3/question_2.jl
+++ Exercise_3/question_2.jl
@@ -3,48 +3,59 @@ function binaryintervalsearch(x,delta, coordinate)
     r = length(x[:,coordinate])
     q = fld((p+r),2)
     
-    if mod1(r, 2) == 2
+    if mod1(r, 2) == 2  # Sjekk om median blir heltall eller snitt av de to midterste
         median = (x[q,coordinate] + x[q+1,coordinate])/2
     else
         median = q
     end
 
-    if p <= r
-        #q = fld((p+r),2)
-        if median == x[q,coordinate]
-            return q
-        elseif median < x[q,coordinate]
-            binaryintervalsearch(x[p:q-1,coordinate])
-        else
-            binaryintervalsearch(x[q:r,coordinate])
-        end
+    left = x[p:q,:] # Lag nye lister, uten å ødelegge x
+    right = x[q+1:r,:]
+    @show left
+    @show right
+
+    lower_bound = median - delta
+    upper_bound = median + delta
+    println("Median = ", median,"; Bounds = [",lower_bound,",",upper_bound,"]")
+
+    lower_index = q
+    upper_index = q
+    
+    lower_counter = 0
+    upper_counter = 0
+
+    # Venstre side
+    while lower_bound <= lower_index && lower_index > 0
+
+        lower_index -= 1
+        lower_counter += 1
+        @show lower_index
+        @show lower_counter
     end
-    return -1
+
+    
+    #if lower_index == 0 && upper_index 
+
 end
 
 
 
-
-
-function testfunction(test)
+function testbinaryintervalsearch(test)
     if test == 1
         x = [1 2; 2 3; 3 0; 4 0; 5 1] 
         delta = 1.5 
         coordinate = 1
-        result = binaryintervalsearch(x,delta,coordinate)
-        return result
+        return binaryintervalsearch(x,delta,coordinate)
     elseif test == 2
         x = [1 2; 2 0; 3 3; 4 4]
         delta = 0.25
         coordinate = 1
-        result = binaryintervalsearch(x,delta,coordinate)
-        return result
+        return binaryintervalsearch(x,delta,coordinate)
     elseif test == 3
         x = [1 0; 2 0; 2 0; 3 0; 4 0; 5 0; 5 0]
         delta = 1
         coordinate = 1
-        result = binaryintervalsearch(x,delta,coordinate)
-        return result
+        return binaryintervalsearch(x,delta,coordinate)
     else 
         return -1
     end
