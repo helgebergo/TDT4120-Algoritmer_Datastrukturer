75386aa13c805d4d7fd62b6a194a0e77e5146975
Helge Bergo <helge.bergo@gmail.com>
2019-10-16 16:24:10 +0200

    Rammeverk for oppgave 3. Fungerer ikke.

--- Exercise_7/question_3.jl
+++ Exercise_7/question_3.jl
@@ -1,6 +1,40 @@
-function mincoins(coins, value)
-    # din kode her
+function usegreed(coins)
+  n = length(coins)
+  isgreedy = true
+  for i in 1:n-1
+    if coins[i] % coins[i+1] == 0
+      isgreedy = true
+    else
+      return false
+    end
+  end
+  return isgreedy
 end
+
+function mincoinsgreedy(coins, value)
+  mincoins = 0.0
+  i = 1
+  while value > 0
+    rest = value % coins[i]
+    mincoins += fld(value,coins[i])
+    value = rest
+    i += 1
+  end
+  return mincoins
+end
+
+function mincoins(coins, value)
+  greedy = usegreed(coins)
+  if greedy
+    mincoins = mincoinsgreedy(coins,value)
+  else
+    mincoins = 0.0
+    i = 1
+  end
+  return mincoins
+end
+
+
 
 ### Tester ###
 # Disse testene blir kjørt når du kjører filen
